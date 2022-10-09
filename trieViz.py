91c0f0841ca8a5316b340a2c084c08b2de391158
madflash <backbugkaught@gmail.com>
2022-10-09 20:35:45 +0530

    style: Format with black

--- trieViz.py
+++ trieViz.py
@@ -4,13 +4,14 @@ from typing import List
 
 import graphviz
 
+
 class Trie:
     def __init__(self, isEOW: bool = False):
         self.isEOW: bool = isEOW
-        self.childs: List['Trie'] = [None] * 256
+        self.childs: List["Trie"] = [None] * 256
 
     def insert(self, word: str) -> None:
-        def mInsert(root: 'Trie', word: str) -> None:
+        def mInsert(root: "Trie", word: str) -> None:
             if len(word) == 0:
                 return
 
@@ -32,7 +33,7 @@ class Trie:
 
     def search(self, word: str) -> bool:
         pass
-        
+
     def startsWith(self, prefix: str) -> bool:
         pass
 
@@ -42,31 +43,37 @@ class Trie:
     def childIdxToAscii(idx) -> str:
         return chr(idx)
 
-    def getDigraph(self, filename: str = 'trieViz.gv'):
-        digraph = graphviz.Digraph("trie", filename = filename)
+    def getDigraph(self, filename: str = "trieViz.gv"):
+        digraph = graphviz.Digraph("trie", filename=filename)
         iterIdx: int = 0
 
-        def dumpTrie(parentIterIdx: int, childs: List['Trie']):
+        def dumpTrie(parentIterIdx: int, childs: List["Trie"]):
             nonlocal iterIdx, digraph
 
             for idx, c in enumerate(childs):
                 if c is not None:
                     iterIdx += 1
-                    digraph.edge(f'Node_{parentIterIdx}', f'Node_{iterIdx}', label=f'{Trie.childIdxToAscii(idx)}')
-                    digraph.node(f'Node_{iterIdx}', label='')
+                    digraph.edge(
+                        f"Node_{parentIterIdx}",
+                        f"Node_{iterIdx}",
+                        label=f"{Trie.childIdxToAscii(idx)}",
+                    )
+                    digraph.node(f"Node_{iterIdx}", label="")
                     dumpTrie(iterIdx, c.childs)
 
-        digraph.node(f'Node_0', label = 'START')
+        digraph.node(f"Node_0", label="START")
         dumpTrie(0, self.childs)
         return digraph
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-        
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('args', nargs=argparse.REMAINDER)
-    parser.add_argument('stdin', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument("args", nargs=argparse.REMAINDER)
+    parser.add_argument(
+        "stdin", nargs="?", type=argparse.FileType("r"), default=sys.stdin
+    )
     args = parser.parse_args().args
 
     if not sys.stdin.isatty():
@@ -79,5 +86,6 @@ def main() -> None:
     digraph = root.getDigraph()
     digraph.view()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
